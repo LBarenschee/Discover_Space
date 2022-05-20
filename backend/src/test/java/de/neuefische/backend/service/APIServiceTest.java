2829668fbcbb2064e781f88267e4e3b3c51ef6f3
LBarenschee <barenschee@gmail.com>
2022-05-20 12:15:52 +0200

    changed the test according to Daniel's idea. Works.

--- backend/src/test/java/de/neuefische/backend/service/APIServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/APIServiceTest.java
@@ -1,19 +1,23 @@
 package de.neuefische.backend.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class APIServiceTest {
 
+    private APIService apiService;
+    private final ExchangeFunction exchangeFunction = mock(ExchangeFunction.class);
 
-    @Test
-    public void checkIf_apiKey_isValid(){
-        //GIVEN
+    @BeforeEach
+    void init() {
+        WebClient webClient = WebClient.builder()
+                .exchangeFunction(exchangeFunction)
+                .build();
 
-        //WHEN
-
-        //THEN
     }
-
 }

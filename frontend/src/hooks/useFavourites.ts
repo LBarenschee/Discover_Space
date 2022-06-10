8ff7f8a7db734a637f01a28739e9f5ba01328467
LBarenschee <barenschee@gmail.com>
2022-06-10 12:50:36 +0200

    Multi user security / login

--- frontend/src/hooks/useFavourites.ts
+++ frontend/src/hooks/useFavourites.ts
@@ -1,24 +1,27 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {NasaPicture} from "../model/NasaPicture";
 import {deleteFavourite, getFavourites, postFavourites} from "../service/api-service";
+import {toast} from "react-toastify";
+import {AuthContext} from "../context/AuthProvider";
 
 export default function useFavourites(){
     const [favouritePictures, setFavouritePictures] = useState<NasaPicture[]>([])
+    const {token} = useContext(AuthContext)
 
     useEffect(()=>{
-        getFavourites()
+        getFavourites(token)
             .then(favouritePictures => setFavouritePictures(favouritePictures))
-            .catch(()=> console.error("Can't show Favourites"))
-    }, [])
+            .catch(()=> toast.error("Can't show Favourites"))
+    }, [token])
 
     const makeFavourite =  (nasaPicture : NasaPicture) =>{
-        postFavourites(nasaPicture)
+        postFavourites(nasaPicture, token)
             .then((nasaPicture)=> {setFavouritePictures([...favouritePictures, nasaPicture])
             })
     }
 
     const removeFavourite = (id: string) =>{
-        deleteFavourite(id)
+        deleteFavourite(id, token)
             .then(()=>setFavouritePictures(favouritePictures.filter(picture => picture.id !==id)))
             .catch(() => console.error())
     }
--- frontend/src/hooks/useRandomPicture.ts
+++ frontend/src/hooks/useRandomPicture.ts
@@ -1,13 +1,16 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {getRandomPicture} from "../service/api-service";
 import {NasaPicture} from "../model/NasaPicture";
+import {AuthContext} from "../context/AuthProvider";
 
 export default function useRandomPicture() {
     const [randomPicture, setRandomPicture] = useState<NasaPicture> ();
+    const {token} = useContext(AuthContext);
+
     useEffect (() => {
-        getRandomPicture ()
+        getRandomPicture (token)
             .then (randomPicture => setRandomPicture (randomPicture))
             .catch (() => console.error ("Can't find Random Picture"))
-    }, [])
+    }, [token])
     return randomPicture
 }
